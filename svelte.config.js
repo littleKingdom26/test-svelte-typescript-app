58e6382cb6c910ac6bffbe115eb6ae2a5c25e124
taeho <wingindream@gmail.com>
2022-03-11 16:49:55 +0900

    Initial commit

---
+++ svelte.config.js
@@ -0,0 +1,21 @@
+// svelte.config.js
+const sveltePreprocess = require('svelte-preprocess');
+const autoprefixer = require("autoprefixer");
+
+const production = !process.env.ROLLUP_WATCH;
+
+module.exports = {
+	preprocess: sveltePreprocess({
+		sourceMap: !production,
+		scss:{
+			prependData : `@import "src/assets/scss/variables.scss";`
+		},
+		postcss: {
+			plugins: [autoprefixer()]
+		}
+	}),
+	compilerOptions: {
+		// enable run-time checks when not in production
+		dev: !production
+	}
+};
